fd975c4bcd11811cd643161559e9d083f9e158a2
chengupc <33085512+chengupc@users.noreply.github.com>
2017-12-19 21:42:35 +0800

    Update step7.py

--- step7.py
+++ step7.py
@@ -4,7 +4,7 @@ Created on Tue Dec 12 10:18:07 2017
 
 @author: Administrator
 """
-
+#进行排序选出最大的值
 import numpy as np
 
 Fre = np.load('Fre.npy')
@@ -17,4 +17,18 @@ for f, co in zip(Fre,ver_CO):
     data_CO.append((co[ind_co[:1]],f[ind_co[:1]]))
     
 np.save('CO_sorted.npy',data_CO)
-    
+   
+#筛选最大值占比例大于0.8的数
+
+import numpy as np
+
+CO = np.load('CO_sorted.npy')
+
+CO_08 = []
+index = []
+for i, x in enumerate(CO):
+    if x[0] > 0.8:
+        index.append(i)
+        CO_08.append(x)
+np.save('indi.npy',np.array(index))
+np.save('Pro08.npy',np.array(CO_08)[:,1])
