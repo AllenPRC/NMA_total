b19ca556c137e8bd09fdcc9e430c4c7df1aa0dac
chengupc <33085512+chengupc@users.noreply.github.com>
2017-12-19 20:07:01 +0800

    Update step4.py

--- step4.py
+++ step4.py
@@ -11,4 +11,20 @@ for i, x in enumerate(a):
 print(n)
 np.save('indi.npy',indi)
 
+#进行数据清新
+
+import numpy as np
+from sys import argv
+
+script, old_data, new_data = argv
+
+ind = np.load('indi.npy')
+o_data = np.load(old_data)
+
+n_data = []
+
+for i in ind:
+    n_data.append(o_data[i])
+
+np.save(new_data,n_data)
 
