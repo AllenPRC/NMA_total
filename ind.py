21e6f462415b945e43c2b5ff4be9a5e0c5ca45af
chengupc <33085512+chengupc@users.noreply.github.com>
2017-12-19 21:06:45 +0800

    Add files via upload

---
+++ ind.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.load('Params.npy')
+n = 0
+indi = []
+for i, x in enumerate(a):
+    if (abs(x[16]) > 170) :
+        indi.append(i)
+        n += 1
+print(n)
+np.save('indi.npy',indi)
